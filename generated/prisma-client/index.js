3ae6a52efb0b19485b7d920af1e68cc9077b6692
tailung1004 <tailung1004@gmail.com>
2022-03-16 23:47:03 +0100

    first commit

---
+++ generated/prisma-client/index.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "Recipe",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `https://eu1.prisma.sh/bkuriakb/react-prisma-recipe-app/dev`
+});
+exports.prisma = new exports.Prisma();
